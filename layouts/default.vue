22ed3ce484e0677da6025535973d8f77f338afbf
hanczkamartanatalia <126960176+hanczkamartanatalia@users.noreply.github.com>
2025-04-20 19:08:02 +0200

    titles

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,9 +11,9 @@ const midList = [
     index: 0,
     path: "/visit/list",
     icon: "material-symbols:format-list-bulleted",
-    name: "VISIT",
+    name: "VISITS",
   },
-  { index: 1, path: "/pet/list", icon: "material-symbols:pets" },
+  { index: 1, path: "/pet/list", icon: "material-symbols:pets", name: "PETS" },
 ];
 
 const bottomList = [
@@ -48,21 +48,21 @@ const listItem = [{
       <div class="">
         <div v-for="item in topList" :key="item.index">
           <nuxt-link :to="item.path" active-class="active-link">
-            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" />
+            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" :title="$t(item.name)" />
           </nuxt-link>
         </div>
       </div>
       <div class="flex-fill d-flex flex-column justify-content-center">
         <div v-for="item in midList" :key="item.index" class="">
           <nuxt-link :to="item.path" active-class="active-link">
-            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" />
+            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" :title="$t(item.name)" />
           </nuxt-link>
         </div>
       </div>
       <div class="">
         <div v-for="item in bottomList" :key="item.index">
           <nuxt-link :to="item.path" active-class="active-link">
-            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" />
+            <Icon :name="item.icon" class="icon --btn fs-3 my-1 --a-increase" :title="$t(item.name)" />
           </nuxt-link>
         </div>
         <DropdownsWithIcon title="SETTING" :list_item="listItem" icon="material-symbols:settings-heart" />
